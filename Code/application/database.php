83e23eef39c57e2bdd477e9bb1719d6906106a6a
yan <1277218464@qq.com>
2020-03-16 15:41:49 +0800

    优化

--- Code/application/database.php
+++ Code/application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '47.106.188.177',
+    'hostname'        => '106.52.247.35',
     // 数据库名
-    'database'        => 'photo',
+    'database'        => 'cheng',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'chengdan',
     // 密码
-    'password'        => '1234567',
+    'password'        => 'CHENGdan123+',
     // 端口
     'hostport'        => '',
     // 连接dsn
